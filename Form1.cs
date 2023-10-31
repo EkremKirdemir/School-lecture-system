55b5087286c0fb14636b7621b33d17c4b4af71e1
EkremKirdemir <kirdemir.ekrem@gmail.com>
2023-10-31 16:26:33 +0300

    Proje dosyası ekle.

---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yazlab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public bool VerifyLogin(string role,string username,string password)
+        {
+            if (username == "" && password == "")
+            {
+                if (role == "student")
+                {
+                    teacherUserControl1.Visible = false;
+                    studentUserControl1.Visible = true;
+                }
+                else if (role == "teacher")
+                {
+                    teacherUserControl1.Visible = true;
+                    studentUserControl1.Visible = false;
+                }
+                else if (role == "admin")
+                {
+                    teacherUserControl1.Visible = false;
+                    studentUserControl1.Visible = false;
+                    adminUserControl1.Visible = true;
+                }
+                return true;
+            }
+            else
+                return false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string role = radioButton1.Checked
+        ? "student"
+        : radioButton2.Checked
+        ? "teacher"
+        : radioButton3.Checked
+        ? "admin"
+        : "";
+
+
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            if (VerifyLogin(role, username, password))
+            {
+                MessageBox.Show("Login successful!");
+                
+            }
+            else
+            {
+                MessageBox.Show("Login failed. Please check your credentials.");
+            }
+
+        }
+    }
+}
---
+++ StudentUserControl.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Pdf;
+using Spire.Pdf.Exporting.Text;
+
+namespace yazlab
+{
+    public partial class StudentUserControl : UserControl
+    {
+        public StudentUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentUserControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        List<CourseData> SplitTranscript(string text)
+        {
+            List<CourseData> courseData = new List<CourseData>();
+
+            string[] splittedLines = text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            for (int i = 0; i < splittedLines.Length; i++)
+            {
+                if (splittedLines[i].EndsWith("(Comment)"))
+                {
+                    int x = i + 1;
+                    while (x < splittedLines.Length && !splittedLines[x].Trim().StartsWith("DNO"))
+                    {
+                        //string[] a = splittedLines[x].Split(' ');
+                        string[] a = System.Text.RegularExpressions.Regex.Split(splittedLines[x], @"\s+");
+
+                        List<string> course = new List<string>();
+
+                        if (a.Length > 1)
+                        {
+                            course.Add(a[1]);
+                            string courseName = "";
+                            for (int j = 2; j < a.Length; j++)
+                            {
+                                if (a[j] != "Z" && a[j] != "S")
+                                {
+                                    courseName += " " + a[j];
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                            course.Add(courseName.Trim());
+                            if (a.Length >= 3)
+                            {
+                                course.Add(a[a.Length - 3]);
+                            }
+                            courseData.Add(new CourseData
+                            {
+                                CourseCode = course[0],
+                                CourseName = course[1],
+                                CourseCredit = course.Count >= 3 ? course[2] : ""
+                            });
+                        }
+                        x += 2;
+                    }
+                }
+            }
+            return courseData;
+        }
+
+        private void OCR(string pdfFilePath)
+        {
+            PdfDocument pdfDocument = new PdfDocument();
+            pdfDocument.LoadFromFile(pdfFilePath);
+
+            foreach (PdfPageBase pdfPage in pdfDocument.Pages)
+            {
+                string pageText = pdfPage.ExtractText();
+                List<CourseData> courseData = SplitTranscript(pageText);
+
+                foreach (CourseData course in courseData)
+                {
+                    listBox1.Items.Add(course.CourseCode + "   " + course.CourseName + "   " + course.CourseCredit);
+                    Console.WriteLine(course.CourseCode + "   " + course.CourseName + "   " + course.CourseCredit);
+                }
+            }
+        }
+
+        private void usTranscript_Click(object sender, EventArgs e)
+        { 
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Open File";
+                openFileDialog.Filter = "PDF (*.pdf*)|*.pdf*"; // You can specify a filter to restrict the file types allowed.
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedFilePath = openFileDialog.FileName;
+                    listBox1.Items.Clear();
+                    listBox1.Visible = true;
+                    label1.Text = "Course list";
+                    OCR(selectedFilePath);
+                }
+            }
+        }
+
+        private void demandCourse_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Visible = true;
+        }
+
+        private void sdCourses_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Visible = true;
+            
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            for(int i = checkedListBox1.Items.Count - 1; i >= 0; i--){
+                if (checkedListBox1.GetItemChecked(i))
+                {
+                    checkedListBox1.Items.RemoveAt(i);
+                }
+            }
+            
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Add("showing demanded");
+        }
+    }
+
+    public class CourseData
+    {
+        public string CourseCode { get; set; }
+        public string CourseName { get; set; }
+        public string CourseCredit { get; set; }
+    }
+}
